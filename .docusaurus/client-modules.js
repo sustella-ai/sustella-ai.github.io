638828943e2f743930796f06f90cc43596684680
Arijit Das <arijit.das@selfsupervised.de>
2024-06-19 13:53:35 +0200

    npm build

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/das/CodeProjects/sustella-ai.github.io/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/das/CodeProjects/sustella-ai.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/das/CodeProjects/sustella-ai.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/das/CodeProjects/sustella-ai.github.io/src/css/custom.css"),
+];
